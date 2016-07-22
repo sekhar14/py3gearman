7c6801d47f772a395a37bf2777f6795eb53ca45c
Shuto Shimabukuro <shuto.shimabukuro@i-plug.co.jp>
2016-07-22 17:05:19 +0900

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "python3-gearman",
     version = "0.0.1",
-    packages=['python3-gearman'],
+    # packages=['python3-gearman'],
+    packages=find_packages(),
 )
