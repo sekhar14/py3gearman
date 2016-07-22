f71d97b973507c36bdf9f77325a67f5ab4ffedd0
Shuto Shimabukuro <shuto.shimabukuro@i-plug.co.jp>
2016-07-22 17:16:47 +0900

    修正

--- py3gearman/__init__.py
+++ py3gearman/__init__.py
@@ -4,12 +4,12 @@ Gearman API - Client, worker, and admin client interfaces
 
 __version__ = '2.0.2'
 
-from gearman.admin_client import GearmanAdminClient
-from gearman.client import GearmanClient
-from gearman.worker import GearmanWorker
+from py3gearman.admin_client import GearmanAdminClient
+from py3gearman.client import GearmanClient
+from py3gearman.worker import GearmanWorker
 
-from gearman.connection_manager import DataEncoder
-from gearman.constants import PRIORITY_NONE, PRIORITY_LOW, PRIORITY_HIGH, JOB_PENDING, JOB_CREATED, JOB_FAILED, JOB_COMPLETE
+from py3gearman.connection_manager import DataEncoder
+from py3gearman.constants import PRIORITY_NONE, PRIORITY_LOW, PRIORITY_HIGH, JOB_PENDING, JOB_CREATED, JOB_FAILED, JOB_COMPLETE
 
 import logging
 
@@ -17,5 +17,5 @@ class NullHandler(logging.Handler):
     def emit(self, record):
         pass
 
-gearman_root_logger = logging.getLogger('gearman')
+gearman_root_logger = logging.getLogger('py3gearman')
 gearman_root_logger.addHandler(NullHandler())
