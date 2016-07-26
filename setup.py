9d0138d45358ba0b98671db39b0048e2b93ac576
Shuto Shimabukuro <shuto.shimabukuro@i-plug.co.jp>
2016-07-26 12:01:41 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py3gearman",
-    version="0.1.1",
+    version="0.1.2",
     author="Shuto Shimabukuro",
     author_email="shuto4192@gmail.com",
     description="Gearman API - Client, worker, and admin client interfaces for Python3",
